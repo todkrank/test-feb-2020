988d7f8c5ed7167e9a98bfbb61ee6d6ce2989d24
evilko <evilko@localhost.localdomain>
2020-02-17 20:31:15 +0200

    Added 'readfile' function * changed function name 'generate' to 'generatefile' for consistency * changed writing method in 'generatefile' function * added readfile function * minor consistency fixes

--- main.cpp
+++ main.cpp
@@ -4,33 +4,55 @@
 #include <string>
 #include <climits>
 
-void generate(std::string, int);
+void generatefile(std::string, int);
+void readfile(std::string, int);
 
 int main(){
   int N;
   std::string filename;
   std::cout<<"Enter the filename(without extension) and the number of entries"<<std::endl;
   std::cin>>filename>>N;
-  generate(filename, N);
+  generatefile(filename,N);
+  readfile(filename,N);
   return 0;
 }
 
-void generate(std::string filename, int N){
-  int i=0;
+void generatefile(std::string filename, int N){
+  int i(0);
   int randnum;
   std::ofstream f;
   std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(0,INT_MAX);
+  std::uniform_int_distribution<int> distribution(INT_MIN,INT_MAX);
 
   f.open(filename,std::ios::out|std::ios::binary);
   if(!f.is_open()){
     std::cout<<"Error opening file "<<filename<<std::endl;
     return;
   }
+  std::cout<<"Writing N entries from the "<<filename<<std::endl;
   for(;i<N;++i){
     randnum=distribution(generator);
-    std::cout<<"Writing "<<randnum<<" to "<<filename<<std::endl;
-    f<<randnum;
+    std::cout<<randnum<<std::endl;
+    f.write((char*)&randnum,sizeof(int));
+  }
+  std::cout<<"Closing "<<filename<<" file "<<std::endl;
+  f.close();
+  return;
+}
+
+void readfile(std::string filename, int N){
+  int i(0);
+  int number;
+  std::ifstream f;
+  std::cout<<"Reading N entries from the "<<filename<<std::endl;
+  f.open(filename,std::ios::in|std::ios::binary);
+  if(!f.is_open()){
+    std::cout<<"Error opening file "<<filename<<std::endl;
+    return;
+  }
+  for(;i<N;++i){
+    f.read((char*)&number,sizeof(int));
+    std::cout<<number<<std::endl;
   }
   std::cout<<"Closing "<<filename<<" file "<<std::endl;
   f.close();
