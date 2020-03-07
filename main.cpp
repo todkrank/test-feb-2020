5ce1f84a3a7d716c41f23341678b6dcce2a006af
evilko <evilko@localhost.localdomain>
2020-03-08 00:46:52 +0200

    general code structure changes * declared command parameters out of command while-loop * changed some messages * changed the order of command checks (in if-statements) * added input validity checks * app arguments--argc, argv-- are unused (because commands are entered in the while-loop) and thus were removed

--- main.cpp
+++ main.cpp
@@ -1,48 +1,63 @@
 #include "test-feb-2020-qmake.h"
 
-int main(int argc, char *argv[]){
+int main(void){
   std::string command;
-
+  std::string filename;
+  const size_t MAX_FILENAME_LENGTH(255);
+  int N(0); //make it size_t and change function declr/definitions in the header corresp.
   for(size_t i(0);i<MAXTHREADS;++i){
     threads.push_back(std::thread(waitloop));
   }
 
-  std::cout<<"Launched with "<<argc<<" parameters"<<std::endl;
-  if(argc!=1){//console output access is asynchoronous, outputs gibberish
-    std::cout<<"Launch without parameters, then enter commands.\n"<<
-               "The list of available commands:\n"<<
-               "generate <filename> <N entries>\n"<<
-               "read <filename>\n"<<
-               "exit\n";
-  }
-  while(true){
-    std::cout<<"Command: ";
-    std::cin>>command;
+  //console output access is asynchoronous, outputs gibberish
+  std::cout<<"The list of available commands:\n"<<
+             "generate <filename> <N entries>\n"<<
+             "read <filename>\n"<<
+             "exit\n";
 
-    if(!strcmp(command.c_str(),"generate")){
-      std::cout<<"Generating..."<<std::endl;
-      std::string filename;
-      int N(0);
-      std::cout<<"Filename: ";
-      std::cin>>filename>>N;//input check! .eof() || .bad() + cin.ignore();
-      std::function<void()> f_lambda=[filename,N](){generatefile(filename,N);};
-      pushtask(f_lambda);
-    }
-    if(!strcmp(command.c_str(),"read")){
-      std::cout<<"Reading..."<<std::endl;
-      std::string filename;
-      std::cin>>filename;//input check! .eof() || .bad() + cin.ignore();
-      std::function<void()> f_lambda=[filename](){readfile(filename);};
-      pushtask(f_lambda);
-    }
+  while(true){
+    std::cin>>command;
     if(!strcmp(command.c_str(),"exit")){
       std::cout<<"Exiting..."<<std::endl;
       continuecondition=false;
       condition.notify_all();
       break;
     }
-    std::cout<<"Command not recognised\n";//will trigger ALWAYS
+    if(!strcmp(command.c_str(),"read")){
+      if(!(std::cin>>filename)){//if the input is invalid (cin returns bad() or eof()
+        std::cout<<"Invalid filename. Repeat command:"<<std::endl;
+        continue;
+      }
+      if(filename.length()>MAX_FILENAME_LENGTH){
+        std::cout<<"Filename too long. Repeat command:"<<std::endl;
+        continue;
+      }
+      std::cout<<"Reading "<<filename<<std::endl;
+      std::function<void()> f_lambda=[filename](){readfile(filename);};
+      pushtask(f_lambda);
+    }
+    if(!strcmp(command.c_str(),"generate")){
+      if(!(std::cin>>filename)){//if the input is invalid (cin returns bad() or eof()
+        std::cout<<"Invalid filename. Repeat command:"<<std::endl;
+        continue;
+      }
+      if(filename.length()>MAX_FILENAME_LENGTH){
+        std::cout<<"Filename too long. Repeat command:"<<std::endl;
+        continue;
+      }
 
+
+      if(!std::cin>>N){//is N unsigned already?
+        std::cout<<"Invalid number. Repeat command:"<<std::endl;
+        continue;
+      }
+      std::cout<<"Generating "<<filename<<" with "<<N<<" numbers"<<std::endl;
+      std::function<void()> f_lambda=[filename,N](){generatefile(filename,N);};
+      pushtask(f_lambda);
+    }
+    else {
+      std::cout<<"Command not recognised\n";
+    }
   }
 //add a check that excludes the same filenames during the same session
 
