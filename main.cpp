f0669c94afa5cf3aab7275f2517dc8378aa6611a
evilko <evilko@localhost.localdomain>
2020-03-14 14:16:45 +0200

    added c-style file operations functions and statistical f-ctions
    * added generatefilec, readfilec
    ? should cout, cin be replaced with (f)printf, (f)scanf respectively?
    * edited README.md, added checkmarks. It looks nice
    * minor main.cpp code cleanup
    * added filestats f-ction that reads numbers and outputs mean and variance
    ? replace pass-by-value with pass-by-reference in mean(), variance()?
    ! replace old-style casts
    * changed commit format, now adding an empty line after the title

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include "test-feb-2020-qmake.h"
-
+//1.put filestats .h in order
+//2.generate - random/hand input; bin/text/(toml?), add one more parameter - flags?
+//3.create hand input txt file with 2 4 4 4 5 5 7 9 numbers (mean 5, var 4, stdev 2)
+//4.add delays into filestats to make testing easier
 int main(void){
   std::string command;
   std::string filename;
   const size_t MAX_FILENAME_LENGTH(255);
-  int N(0); //make it size_t and change function declr/definitions in the header corresp.
+  size_t N(0);
   for(size_t i(0);i<MAXTHREADS;++i){
     threads.push_back(std::thread(waitloop));
   }
 
-  //console output access is asynchoronous, outputs gibberish
+  //console output access is asynchoronous, outputs gibberish without mutex
   std::cout<<"The list of available commands:\n"<<
              "generate <filename> <N entries>\n"<<
              "read <filename>\n"<<
+             "filestats <filename>\n"<<
              "exit\n";
 
   while(true){
+    //1.replace generatefile with c-version
     std::cin>>command;
     if(!strcmp(command.c_str(),"exit")){
       std::cout<<"Exiting..."<<std::endl;
@@ -32,9 +37,9 @@ int main(void){
         std::cout<<"Filename too long. Repeat command:"<<std::endl;
         continue;
       }
-      std::cout<<"Reading "<<filename<<std::endl;
-      std::function<void()> f_lambda=[filename](){readfile(filename);};
+      std::function<void()> f_lambda=[filename](){readfilec(filename.c_str());};
       pushtask(f_lambda);
+      continue;
     }
     if(!strcmp(command.c_str(),"generate")){
       if(!(std::cin>>filename)){//if the input is invalid (cin returns bad() or eof()
@@ -45,21 +50,33 @@ int main(void){
         std::cout<<"Filename too long. Repeat command:"<<std::endl;
         continue;
       }
-
-
-      if(!std::cin>>N){//is N unsigned already?
+      if(!(std::cin>>N)){
         std::cout<<"Invalid number. Repeat command:"<<std::endl;
         continue;
       }
-      std::cout<<"Generating "<<filename<<" with "<<N<<" numbers"<<std::endl;
-      std::function<void()> f_lambda=[filename,N](){generatefile(filename,N);};
+
+      std::function<void()> f_lambda=[filename,N](){generatefilec(filename.c_str(),N);};
       pushtask(f_lambda);
+      continue;
     }
-    else {
-      std::cout<<"Command not recognised\n";
+    if(!strcmp(command.c_str(),"filestats")){
+      if(!(std::cin>>filename)){//if the input is invalid (cin returns bad() or eof()
+        std::cout<<"Invalid filename. Repeat command:"<<std::endl;
+        continue;
+      }
+      if(filename.length()>MAX_FILENAME_LENGTH){
+        std::cout<<"Filename too long. Repeat command:"<<std::endl;
+        continue;
+      }
+      std::cout<<"Reading "<<filename<<std::endl;
+      std::function<void()> f_lambda=[filename](){filestats(filename.c_str());};
+      pushtask(f_lambda);
+      continue;
     }
+    std::cout<<"Command not recognised\n";
   }
 //add a check that excludes the same filenames during the same session
+//incapsulate code in if's? is it possible
 
   for(size_t i(0);i<MAXTHREADS;++i){
     threads.at(i).join();
--- test-feb-2020-qmake.h
+++ test-feb-2020-qmake.h
@@ -2,6 +2,7 @@
 #define TESTFEB2020QMAKE_H
 
 #include <iostream>
+#include <cstdio>
 #include <fstream>
 #include <random>
 #include <string>
@@ -16,21 +17,32 @@
 #include <functional>
 #include <mutex>
 
+// Locks, threads, conditions - concurrency
 const size_t MAXTHREADS(std::thread::hardware_concurrency());
 static std::vector<std::thread> threads;
 static std::queue<std::function<void()>> tasks;
 static std::atomic<bool> continuecondition(true);
 
 static std::mutex tasksmutex;
+static std::mutex coutmutex;
 static std::condition_variable condition;
 
-void generatefile(std::string, int);
+// File operations
+void generatefile(std::string, size_t);
+void generatefilec(const char*, size_t);
 void readfile(std::string);
+void readfilec(const char *);
+
+// Threadpool functions
 void waitloop();
 void pushtask(std::function<void()>);
 
-void generatefile(std::string filename, int N){
-  int i(0);
+// Statistical functions, step-by-step
+double mean(const double, const double, const double);
+double variance(const double, const double, const double, const double);
+
+void generatefile(std::string filename, size_t N){
+  size_t i(0);
   int randnum;
   std::ofstream f;
   std::default_random_engine generator;
@@ -91,6 +103,99 @@ void pushtask(std::function<void()> newtask){
     tasks.push(newtask);
   }
   condition.notify_one();
+  return;
+}
+
+void generatefilec(const char* filename, size_t N){
+  size_t i(0);
+  int randnum;
+  FILE *f;
+  f=fopen(filename,"wb");
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(INT_MIN,INT_MAX);
+
+  if(!f){//is this okay? or f==nullptr?
+    std::cout<<"Error opening file "<<filename<<std::endl;
+    return;
+  }
+  std::cout<<"Writing "<<N<<" entries to \'"<<filename<<"\'"<<std::endl;
+  for(;i<N;++i){
+    randnum=distribution(generator);
+    std::cout<<randnum<<std::endl;
+    fwrite(&randnum,sizeof(int),1,f);
+  }
+  std::cout<<"Closing "<<filename<<" file "<<std::endl;
+  fclose(f);
+  return;
+}
+
+void readfilec(const char *filename){
+  int number;
+  FILE* f;
+  std::cout<<"Reading from \'"<<filename<<"\'"<<std::endl;
+  f=fopen(filename,"rb");
+  if(!f){
+    std::cout<<"Error opening file "<<filename<<std::endl;
+    return;
+  }
+  while(fread(&number,sizeof(int),1,f)){
+    std::cout<<number<<std::endl;
+  }
+  std::cout<<"Closing "<<filename<<" file "<<std::endl;
+  fclose(f);
+  return;
+}
+
+double mean(double an, double n, double prevmean){
+  double m;
+  m=((prevmean*(n-1)+an)/n);
+  return m;
+}
+
+double variance(double an, double n, double prevvariance, double prevmean){
+  double v;
+  v=(n-1)/n*(prevvariance+(an-prevmean)*(an-prevmean)/n);
+  return v;
+}
+
+void filestats(const char* filename){
+  int i(0);
+  int number;
+  double m(0), pm(0), v(0), pv(0);
+  FILE* f;
+  std::cout<<"Reading from \'"<<filename<<"\'"<<std::endl;
+  f=fopen(filename,"rb");
+  if(!f){
+    std::cout<<"Error opening file "<<filename<<std::endl;
+    return;
+  }
+  while(fread(&number,sizeof(int),1,f)){
+    ++i;
+//    std::cout<<number<<std::endl;
+    m=mean(number,i,pm);
+    v=variance(number,i,pv,pm);
+    pm=m;
+    pv=v;
+    if(!(i%100))
+    {
+      std::unique_lock<std::mutex> lock(coutmutex);//preventing concurrent access to console output
+      std::cout<<filename<<"\t"<<
+                 "mean: "<<m<<
+                 "\tvariance: "<<v<<
+                 " ("<<i<<" numbers)"<<std::endl;
+    }
+  }
+
+
+  {
+    std::unique_lock<std::mutex> lock(coutmutex);//preventing concurrent access to console output
+    std::cout<<filename<<"\t"<<
+               "mean: "<<m<<
+               "\tvariance: "<<v<<
+               " ("<<i<<" numbers)"<<std::endl;
+  }
+  std::cout<<"Closing "<<filename<<" file "<<std::endl;
+  fclose(f);
   return;
 }
 
